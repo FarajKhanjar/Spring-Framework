13a87b43e7dd9835e999821a0444193f75eef600
FarajKhanjar <farajkhanjar@gmail.com>
2022-07-03 09:24:10 +0300

    First connection.

--- bootDemo/src/main/java/ajbc/learn/springboot/bootDemo/BootDemoApplication.java
+++ bootDemo/src/main/java/ajbc/learn/springboot/bootDemo/BootDemoApplication.java
@@ -2,12 +2,22 @@ package ajbc.learn.springboot.bootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class BootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootDemoApplication.class, args);
+	}
+	
+	@GetMapping("hello")
+	public void sayHello() {
+		System.out.println("Hi - connented!");
 	}
 
 }
