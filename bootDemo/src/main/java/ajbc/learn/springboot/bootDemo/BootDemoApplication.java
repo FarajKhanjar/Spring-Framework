fd75f9d6be189338f0ed60c3a8306389094c2005
FarajKhanjar <farajkhanjar@gmail.com>
2022-07-03 09:09:23 +0300

    Create springBoot demo.

---
+++ bootDemo/src/main/java/ajbc/learn/springboot/bootDemo/BootDemoApplication.java
@@ -0,0 +1,13 @@
+package ajbc.learn.springboot.bootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemoApplication.class, args);
+	}
+
+}
